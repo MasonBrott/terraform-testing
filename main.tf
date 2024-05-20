369d61346561388ec8611cbc68478d751887cd50
mason <masonbrott@gmail.com>
2024-05-20 14:59:49 -0500

    setting up actions

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+    backend "gcs" {}
+    provider "google" {
+    project = " test-environment-423919"
+    region = "us-central1"
+    }   
+}
+
+required_providers {
+    google = {
+      version = ">=5.15.0"
+    }
+}
+
+provider "google" {
+    project = "test-environment-423919"
+    region = "us-central1"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
